e210e5cd398fada3946dad41dbbbdc304aeb499b
mariozivkovic <mario.zivkovic.znr@gmail.com>
2022-12-20 11:45:50 +0100

    Varijable, primitivni tipovi, omotači, operatori, program

---
+++ Java/eclipse/Zivkovic01/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author lovre
+ *
+ */
+module Zivkovic {
+	requires java.desktop;
+}
---
+++ Java/eclipse/Zivkovic01/zadatci/Z03.java
@@ -0,0 +1,18 @@
+package zadatci;
+
+public class Z03 {
+	
+	// Za unesena dva cijela broja
+		// ispišite rezutat djeljenja
+	
+	public static void main(String[] args) {
+		
+		int a= 8, b= 4;
+		
+		int c = a/b;
+		
+		System.out.println(c);
+		
+	}
+
+}
