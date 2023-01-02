8119786ff20d68085c1dab633e2b6924848278f5
mariozivkovic <mario.zivkovic.znr@gmail.com>
2023-01-02 09:17:15 +0100

    ZZ02- Tablica množenja 15x15

---
+++ Java/eclipse/Zivkovic03/src/zadatci/ZZ02.java
@@ -0,0 +1,25 @@
+package zadatci;
+
+public class ZZ02 {
+
+	// Kreirati program koji ispisuje tablicu množenja 15x15
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 15; i++) {
+			for (int j = 1; j <= 15; j++) {
+				if ((i * j < 10)) {
+					System.out.print(i * j + "   ");
+				} else if ((i * j < 100)) {
+					System.out.print(i * j + "  ");
+				} else if ((i * j > 10) && (i * j < 100)) {
+					System.out.print(i * j + "   ");
+				} else {
+					System.out.print(i * j + " ");
+				}
+
+			}
+			System.out.println();
+		}
+	}
+}
