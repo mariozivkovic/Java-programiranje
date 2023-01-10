0b649d6149c0f94a2e867513e85e8de6e817a6a7
mariozivkovic <mario.zivkovic.znr@gmail.com>
2023-01-10 09:17:20 +0100

    Dorada koda

--- Java/eclipse/Zivkovic03/src/zadatci/TablicaMnozenja.java
+++ Java/eclipse/Zivkovic03/src/zadatci/TablicaMnozenja.java
@@ -28,77 +28,27 @@ public class TablicaMnozenja {
 		System.out.println();
 		
 
-		for (int i = 1; i < 10; i++) {
-			for (int j = 1; j < 10; j++) {
-				if ((i * j < 10)) {
-					if (j == 1) {
-						System.out.print( i + " |  " + i * j + "  ");
-					} else {
-						System.out.print(i * j + "  ");
-					}
-				} else if ((i * j < 100)) {
-					System.out.print(i * j + " ");
-				} else if ((i * j < 10) && (i * j < 100)) {
-					System.out.print(i * j + "");
-				} else {
-					System.out.print(i * j + " ");
-				}
-			}
-			if (i == 9) {
-				System.out.println();
-				for (int k = 0; k < 31; k++) {
-					System.out.print("-");
-				}
-				System.out.println();
-				
-				switch (ime.length()) {
-				case 2:
-					System.out.println(":  :  :  :  :  :   :  :  :by " + ime);
-					break;
-					
-				case 3:
-					System.out.println(":  :  :  :  :  :   :  :  by " + ime);
-					break;
-					
-				case 4:
-					System.out.println(":  :  :  :  :  :   :  : by " + ime);
-					break;
-					
-				case 5:
-					System.out.println(":  :  :  :  :  :   :  :by " + ime);
-					break;
-					
-				case 6:
-					System.out.println(":  :  :  :  :  :   :  by " + ime);
-					break;
-					
-				case 7:
-					System.out.println(":  :  :  :  :  :   : by " + ime);
-					break;
-					
-				case 8:
-					System.out.println(":  :  :  :  :  :   :by " + ime);
-					break;
-					
-				case 9:
-				    System.out.println(":  :  :  :  :  :   by " + ime);
-					break;
-					
-				case 10:
-					System.out.println(":  :  :  :  :  :  by " + ime);
-					break;
-					
-				case 11:
-					System.out.println(":  :  :  :  :  : by " + ime);
-					break;
-				}
-				
-				for (int k = 0; k < 31; k++) {
-					System.out.print("-");
-				}
-			}
-			System.out.println();
+		int a[][] = new int[10][10];
+
+		for (int b = 1; b < 10; b++) {
+			System.out.printf("%3d |", b);
+			for (int c = 1; c < 10; c++) {
+				System.out.printf("%3d", (b * c));
 		}
+			System.out.println();
+	}
+		for (int b = 0; b < 32; b++) {
+			System.out.print("-");
+		}
+		System.out.println();
+		System.out.printf("%32s\n", ": : : : : : : : : : : by Mario");
+		
+
+		for (int b = 0; b < 32; b++) {
+			System.out.print("-");
+		}
+		
+		
 	}
 
 }
