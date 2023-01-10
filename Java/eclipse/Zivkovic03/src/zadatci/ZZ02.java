56eebf96c8cd0aba2d661e702f449fa8e89ca684
mariozivkovic <mario.zivkovic.znr@gmail.com>
2023-01-10 17:47:17 +0100

    Dorada koda

--- Java/eclipse/Zivkovic03/src/zadatci/ZZ02.java
+++ Java/eclipse/Zivkovic03/src/zadatci/ZZ02.java
@@ -6,18 +6,9 @@ public class ZZ02 {
 
 	public static void main(String[] args) {
 
-		for (int i = 1; i <= 15; i++) {
-			for (int j = 1; j <= 15; j++) {
-				if ((i * j < 10)) {
-					System.out.print(i * j + "   ");
-				} else if ((i * j < 100)) {
-					System.out.print(i * j + "  ");
-				} else if ((i * j > 10) && (i * j < 100)) {
-					System.out.print(i * j + "   ");
-				} else {
-					System.out.print(i * j + " ");
-				}
-
+		for(int i=1;i<=15;i++) {
+			for(int j=1;j<=15;j++) {
+				System.out.printf("%4d" , i*j);
 			}
 			System.out.println();
 		}
