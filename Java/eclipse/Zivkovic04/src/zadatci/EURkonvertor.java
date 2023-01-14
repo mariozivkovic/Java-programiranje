4f005efcc1ca43a066f6c0b9109ad515fa3b8f54
mariozivkovic <mario.zivkovic.znr@gmail.com>
2023-01-14 13:01:18 +0100

    Dorada koda

--- Java/eclipse/Zivkovic04/src/zadatci/EURkonvertor.java
+++ Java/eclipse/Zivkovic04/src/zadatci/EURkonvertor.java
@@ -10,10 +10,10 @@ public class EURkonvertor {
 		
 		double tecaj = 7.5345;
 		
-		double iznos = Double.parseDouble(JOptionPane.showInputDialog("Iznos u" + vrsta=="1" ? "EUR" : "KN"));
-		System.out.println(iznos * tecaj + "KN");
+		double iznos = Double.parseDouble(JOptionPane.showInputDialog("Iznos u" + (vrsta.equals("1") ? "EUR" : "KN")));
 		
-		if(vrsta=="1") {
+		
+		if(vrsta.equals("1")) {
 			System.out.println(iznos * tecaj + " KN");
 		}else {
 			System.out.println(iznos / tecaj + " EUR");
