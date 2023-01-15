5bdac9aee038c06a69431ce4ccf9120164f016bc
mariozivkovic <mario.zivkovic.znr@gmail.com>
2023-01-15 11:00:10 +0100

    Riješen zadatak

---
+++ Java/eclipse/Zivkovic05/src/zadatci/Z03_MetodaPrimaImeiPrezimeTeVracaBrojZnakova.java
@@ -0,0 +1,17 @@
+package zadatci;
+
+public class Z03_MetodaPrimaImeiPrezimeTeVracaBrojZnakova {
+
+	// Metoda prima ime i prezime
+		// Metoda vraća ukupan zbroj znakova
+		// primljenih imena i prezimena
+	
+	public static int znakovi (String ime, String prezime) {
+		
+		return ime.length() + prezime.length();
+	}
+	
+	// System.out.println(Z03_MetodaPrimaImeiPrezimeTeVracaBrojZnakova.znakovi(null, null));
+	
+	
+}
