6d8591c7ce44c6e1db138156f069fd8329224e46
mariozivkovic <mario.zivkovic.znr@gmail.com>
2023-02-15 10:33:44 +0100

    Uvod u netbeans

---
+++ Java/netbeans/HelloWorld/src/main/java/edunova/HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edunova;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author lovre
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Osoba osoba = new Osoba();
+        osoba.setIme("Pero");
+        osoba.setPrezime("Perić");
+        
+        System.out.println(osoba.getIme());
+        
+        new Prozor().setVisible(true);
+        
+    }
+}
---
+++ Java/netbeans/HelloWorld/src/main/java/edunova/Osoba.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova;
+
+/**
+ *
+ * @author lovre
+ */
+public class Osoba {
+
+    public Osoba() {
+    }
+    
+    private int sifra;
+    private String ime;
+    private String prezime;
+    protected int stats;
+    boolean uvjet;
+
+    public int getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(int sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+    
+    
+ 
+}
