c6680d61ec4d515afbbf0148a62defd38b6572aa
mariozivkovic <mario.zivkovic.znr@gmail.com>
2023-02-19 10:31:44 +0100

    Riješen zadatak

---
+++ Java/netbeans/BNZ1/src/main/java/zadatak/BNZ1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package zadatak;
+
+/**
+ *
+ * @author lovre
+ */
+public class BNZ1 {
+
+    public static void main(String[] args) {
+       
+        
+        new Prozor().setVisible(true);
+    }
+}
